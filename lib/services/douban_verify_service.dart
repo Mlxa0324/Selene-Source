9c910c1427eae168d2aa51b72ba81e3630c5de1f
Lx <33782374+Mlxa0324@users.noreply.github.com>
2026-01-22 10:23:04 +0800

    优化cookie缓存

--- lib/services/douban_verify_service.dart
+++ lib/services/douban_verify_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:async';
 import 'package:crypto/crypto.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class DoubanVerifyService {
